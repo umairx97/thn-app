7cb2181a9a45f539567465aef3dab9ea615d4412
umairx97 <umair.bajwa@hotmail.com>
2019-01-16 21:31:50 +0500

    Commit

--- src/components/table.js
+++ src/components/table.js
@@ -9,10 +9,10 @@ const Table = (props) => {
         {list.filter(searching(pattern)).map(item => (
           <div key={item.objectID}>
             <span>
-              <a href={item.url}>{item.title}</a>
+              <a href={item.url}>{item.title} </a>
             </span>
             <span>{item.author}</span> <span>{item.num_comments}</span>
-            <span>{item.points}</span>
+            <span>{item.points} </span>
             <span>
               <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
             </span>
