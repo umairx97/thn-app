201e9b3b08dc23996fcb177e06df20e46f7667b1
umairx97 <umair.bajwa@hotmail.com>
2019-01-25 22:54:13 +0500

    Added Paginated fetch #117

--- src/App.js
+++ src/App.js
@@ -43,7 +43,6 @@ class App extends Component {
       .catch(error => error);
   }
 
-  
   componentDidMount() {
     const { searchTerm } = this.state;
     this.fetchSearchTopStories(searchTerm);
